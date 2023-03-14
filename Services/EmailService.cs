5568059223f843b5799f913ecedcfde70479f7fe
ArthurGalanti <arthur.galanti@hotmail.com>
2023-03-13 21:50:01 -0300

    Adicionado Services

---
+++ Services/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace BlogAPI.Services;
+
+public class EmailService
+{
+    public bool Send(
+        string toName,
+        string toEmail,
+        string subject,
+        string body,
+        string fromName = "Arthur Galanti",
+        string fromEmail = "arthur.galanti@hotmail.com")
+    {
+        var smtpClient = new SmtpClient(Configuration.Smtp.Host, Configuration.Smtp.Port);
+        smtpClient.Credentials = new NetworkCredential(Configuration.Smtp.UserName, Configuration.Smtp.Password);
+        smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+        smtpClient.EnableSsl = true;
+        
+        var mail = new MailMessage();
+
+        mail.From = new MailAddress(fromEmail, fromName);
+        mail.To.Add(new MailAddress(toEmail, toName));
+        mail.Subject = subject;
+        mail.Body = body;
+        mail.IsBodyHtml = true;
+
+        try
+        {
+            smtpClient.Send(mail);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex.Message);
+            return false;
+        }
+    }
+}
